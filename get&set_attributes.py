895d48f1754250e5078b95b37da68366be0099fb
Asmita Singh <asingh290704@gmail.com>
2025-07-15 12:36:13 +0530

    get and set items

--- get&set_attributes.py
+++ get&set_attributes.py
@@ -6,6 +6,7 @@ class Numbers:
     def __setitem__(self,index,val):
         self.myList[index] = val
 NumList = Numbers([1,2,3,4,5,6,7,8,9,10])
+print(NumList.myList)
 print(NumList[5])
 NumList[3] = 10
 print(NumList.myList)
